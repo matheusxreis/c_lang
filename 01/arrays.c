e71324832d4e6e238c77c593cb8d659867830566
Matheus Reis <matheus.reis@triibo.com.br>
2023-10-24 13:08:07 -0300

    array of pointers

--- 01/arrays.c
+++ 01/arrays.c
@@ -11,15 +11,29 @@ int main() {
   // dinamic size array
   char *sized;
   sized = malloc(5*sizeof(char *)); // -> heap
+  
+  int n1 = 10;
+  int n2 = 20;
+  int n3 = 30;
+  int n4 = 40;
 
+  int *p_integers[] = {&n1, &n2, &n3, &n4};
 
   for(int i = 0; i <= 5; i++) {
     sized[i] = '1';
     printf("%s ", pointers[i]);
   }
+  
   for(int i = 0; i <= 5; i++) {
     printf("%c", sized[i]);
   }
+
+  for (int i = 0; i <= sizeof(p_integers)/sizeof(&p_integers[0])-1; i++){
+    // printing a pointers array works like that
+    // *n[1] = (base + 1 * esize) -> *(pointer in 1 position) = value;  
+    printf("%c ", (*pointers[i])); //
+    printf("%d ", (*p_integers[i]));
+  };
   
   free(sized);
 };
