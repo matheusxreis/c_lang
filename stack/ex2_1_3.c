932b8bfa3240369628ddc9cc6f888abb1f4bdae2
Matheus Reis <matheus.reis081@gmail.com>
2023-11-16 23:52:18 -0300

    exercises 2.1.3 and 2.1.4

---
+++ stack/ex2_1_3.c
@@ -0,0 +1,69 @@
+/*
+ * The exercise was make an algoritm,
+ * this code is the impl from the algoritm
+ */
+
+#include <stdio.h>
+
+void exec(char str[]){
+  
+  // false stack
+  char push[10];
+  //
+
+  int valid = 1;
+  int middle = 0;
+
+  int count = 2;
+
+  for(int i = 0; str[i]!='\0'; i++) {
+    
+    if(str[i]=='C' && middle==0){
+        middle = 1;
+        continue;
+    };
+
+    if(middle == 0){
+      push[i] = str[i];
+    }else {
+      // imagining that i is not empty
+      char item = push[i - count];
+      if(item != str[i]) {
+        valid = 0;
+        break;
+      }
+      count+=2;
+    };
+  };
+
+  if(valid == 0 || middle == 0){ // or stack is empty
+    printf("String invalid =)\n");
+  }else {
+    printf("String valid =(\n");
+  };
+};
+
+int main() {
+
+  char str1[] = "ABCBA";
+  char str2[] = "ABACABA";
+  char str3[] = "ACA";
+
+  char str4[] ="AA";
+  char str5[] = "AACCBB";
+  char str6[] ="AABBCBBAA";
+  char str7[] = "ACB";
+  char str8[] = "ABABACBABC";
+
+  exec(str1); // expected valid
+  exec(str2); // expected valid
+  exec(str3); // expected valid
+  exec(str6); // expected valid
+  exec(str4); // expected invalid
+  exec(str5); // expected invalid
+  exec(str7);  // expected invalid
+  exec(str8);  // expected invalid
+  
+};
+
+
---
+++ stack/ex2_1_4.c
@@ -0,0 +1,77 @@
+/*
+ * The exercise was make an algoritm,
+ * this code is the impl from the algoritm
+ */
+
+#include<stdbool.h>
+#include <stdio.h>
+
+int exec(char str[]) {
+
+  bool valid = true;
+  // false stack
+  char stack[20];
+  char separator = 'D';
+  bool control = false;
+  int counter = 2;
+
+  int index = -1;
+
+  for(int i = 0; str[i] != '\0'; i++){
+   
+      index+=1;
+     
+      if(str[i] == separator) {
+          control = !control;
+          if(counter>2) { index = 0; };
+          if(index == 0) { counter = 2; };
+          continue;
+      }
+
+      if(!control) {
+        //pushing
+        stack[index] = str[i];
+      }else {
+        // supose that stack is not empty
+        // popping
+        char item = stack[index-counter];
+        if(item != str[i]){
+          valid = false;
+          break;
+        }
+        counter+=2;
+      }
+  }
+
+  if(counter==2 || !valid){ //or stack is not empty
+    printf("String invalid =(\n");
+  }else {
+    printf("String valid =)\n");
+  }
+
+};
+
+int main(){
+
+  char str[] = "ABDBADAADAADBADABDBBDBB"; 
+  char str2[] ="ADADBDBDBADABDBBADABBDABADABA";
+  char str3[] ="ADADBDBDBADABDBBADABBDABAD";
+  char str4[] = "BADAB";
+  char str5[] = "DAADAADBADAB";
+  char str6[] ="D";
+  char str7[] ="AADBBDAADAADBADAB";
+  char str8[] ="DABDBAD";
+  char str9[] = "AADAADAADAADBADAB";
+
+  exec(str); // valid
+  exec(str2); // valid 
+  exec(str3); // invalid
+  exec(str4); // valid
+  exec(str5); // invalid
+  exec(str6); // invalid;
+  exec(str7); // invalid;
+  exec(str8); // invalid
+  exec(str9); // valid;
+
+
+}
